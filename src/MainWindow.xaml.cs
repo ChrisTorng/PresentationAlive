faaf894ed42dc470633140362e0e865e17755ac2
ChrisTorng <christorng@hotmail.com>
2022-06-30 16:07:42 +0800

    Add list, start/next buttons

--- src/MainWindow.xaml.cs
+++ src/MainWindow.xaml.cs
@@ -11,49 +11,66 @@ namespace src
     /// </summary>
     public partial class MainWindow : Window
     {
-        PowerPointApp? app = new()
-        {
-            WindowState = PpWindowState.ppWindowMinimized,
-            Visible = MsoTriState.msoTrue
-        };
+        PowerPointApp app;
         Presentation? presentation;
 
         public MainWindow()
         {
             InitializeComponent();
             this.Closing += MainWindow_Closing;
+
+            this.app = new();
+            //{
+            //    WindowState = PpWindowState.ppWindowMinimized,
+            //    Visible = MsoTriState.msoTrue
+            //};
+            app.SlideShowEnd += this.App_SlideShowEnd;
+
+            this.playList.Items.Add("D:\\Users\\ChrisTorng\\Documents\\個人\\教會\\UP\\20220213\\恢復起初的愛-欣仁版本 - Copy.pptx");
+            this.playList.Items.Add("D:\\Users\\ChrisTorng\\Documents\\個人\\教會\\UP\\20220213\\恢復起初的愛 - Copy.pptx");
+            this.playList.SelectedIndex = 0;
         }
 
         private void MainWindow_Closing(object? sender, System.ComponentModel.CancelEventArgs e)
         {
+            this.presentation?.Close();
+            this.presentation = null;
             this.app?.Quit();
-            this.app = null;
+            this.app = null!;
             GC.Collect();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void ButtonStart_Click(object sender, RoutedEventArgs e)
         {
-            this.presentation = app.Presentations.Open("D:\\Users\\ChrisTorng\\Documents\\個人\\教會\\UP\\20220213\\恢復起初的愛-欣仁版本 - Copy.pptx");
-            var slideShowSettings = presentation.SlideShowSettings;
-            slideShowSettings.Run();
-            app.SlideShowEnd += App_SlideShowEnd1;
+            this.StartSlideShow();
         }
 
-        private void App_SlideShowEnd1(Presentation Pres)
+        private void StartSlideShow()
         {
-            app.SlideShowEnd -= App_SlideShowEnd1;
-            this.presentation?.Close();
-            this.presentation = app.Presentations.Open("D:\\Users\\ChrisTorng\\Documents\\個人\\教會\\UP\\20220213\\恢復起初的愛 - Copy.pptx");
-            var slideShowSettings = presentation.SlideShowSettings;
-            slideShowSettings.Run();
-            app.SlideShowEnd += App_SlideShowEnd2;
+            if (this.playList.SelectedItem != null)
+            {
+                this.presentation = app.Presentations.Open(this.playList.SelectedItem.ToString());
+                var slideShowSettings = presentation.SlideShowSettings;
+                //slideShowSettings.EndingSlide--;
+                slideShowSettings.Run();
+            }
         }
 
-        private void App_SlideShowEnd2(Presentation Pres)
+        private void App_SlideShowEnd(Presentation Pres)
         {
-            app.SlideShowEnd -= App_SlideShowEnd2;
             this.presentation?.Close();
             this.presentation = null;
+
+            if (this.playList.SelectedIndex < this.playList.Items.Count - 1)
+            {
+                this.playList.SelectedIndex++;
+                this.StartSlideShow();
+            }
+        }
+
+        private void ButtonNext_Click(object sender, RoutedEventArgs e)
+        {
+            this.presentation?.SlideShowWindow.View.Next();
         }
     }
 }

89ee568ecb116ede1cc9067d3dc1b61cdeba8f88
ChrisTorng <christorng@hotmail.com>
2022-06-30 11:55:05 +0800

    Show 2 slide show and quit

--- src/MainWindow.xaml.cs
+++ src/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using Microsoft.Office.Core;
 using Microsoft.Office.Interop.PowerPoint;
 using PowerPointApp = Microsoft.Office.Interop.PowerPoint.Application;
@@ -10,22 +11,49 @@ namespace src
     /// </summary>
     public partial class MainWindow : Window
     {
+        PowerPointApp? app = new()
+        {
+            WindowState = PpWindowState.ppWindowMinimized,
+            Visible = MsoTriState.msoTrue
+        };
+        Presentation? presentation;
+
         public MainWindow()
         {
             InitializeComponent();
+            this.Closing += MainWindow_Closing;
+        }
+
+        private void MainWindow_Closing(object? sender, System.ComponentModel.CancelEventArgs e)
+        {
+            this.app?.Quit();
+            this.app = null;
+            GC.Collect();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            var app = new PowerPointApp()
-            {
-                WindowState = PpWindowState.ppWindowMinimized,
-                Visible = MsoTriState.msoTrue
-            };
-            var presentation = app.Presentations.Open("D:\\Users\\ChrisTorng\\Documents\\個人\\教會\\UP\\20220213\\恢復起初的愛-欣仁版本 - Copy.pptx");
+            this.presentation = app.Presentations.Open("D:\\Users\\ChrisTorng\\Documents\\個人\\教會\\UP\\20220213\\恢復起初的愛-欣仁版本 - Copy.pptx");
             var slideShowSettings = presentation.SlideShowSettings;
             slideShowSettings.Run();
+            app.SlideShowEnd += App_SlideShowEnd1;
+        }
 
+        private void App_SlideShowEnd1(Presentation Pres)
+        {
+            app.SlideShowEnd -= App_SlideShowEnd1;
+            this.presentation?.Close();
+            this.presentation = app.Presentations.Open("D:\\Users\\ChrisTorng\\Documents\\個人\\教會\\UP\\20220213\\恢復起初的愛 - Copy.pptx");
+            var slideShowSettings = presentation.SlideShowSettings;
+            slideShowSettings.Run();
+            app.SlideShowEnd += App_SlideShowEnd2;
+        }
+
+        private void App_SlideShowEnd2(Presentation Pres)
+        {
+            app.SlideShowEnd -= App_SlideShowEnd2;
+            this.presentation?.Close();
+            this.presentation = null;
         }
     }
 }

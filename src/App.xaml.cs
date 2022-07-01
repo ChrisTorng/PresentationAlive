3aa019e704fb898fa19e244eb31bf7bb11f34c22
ChrisTorng <ChrisTorng@hotmail.com>
2022-07-01 23:03:56 +0800

    namespace statement

--- src/App.xaml.cs
+++ src/App.xaml.cs
@@ -6,12 +6,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace PresentationAlive
+namespace PresentationAlive;
+
+public partial class App : Application
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
-    {
-    }
 }
--- src/MainWindow.xaml.cs
+++ src/MainWindow.xaml.cs
@@ -5,77 +5,74 @@ using Microsoft.Office.Core;
 using Microsoft.Office.Interop.PowerPoint;
 using PowerPointApp = Microsoft.Office.Interop.PowerPoint.Application;
 
-namespace PresentationAlive
+namespace PresentationAlive;
+
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    PowerPointApp app;
+    Presentation? presentation;
+
+    public MainWindow()
     {
-        PowerPointApp app;
-        Presentation? presentation;
+        this.InitializeComponent();
+        this.Closing += MainWindow_Closing;
 
-        public MainWindow()
+        this.app = new()
         {
-            InitializeComponent();
-            this.Closing += MainWindow_Closing;
+            Visible = MsoTriState.msoTrue,
+            WindowState = PpWindowState.ppWindowMinimized,
+        };
+        app.SlideShowEnd += this.App_SlideShowEnd;
 
-            this.app = new()
+        this.playList.Items.Add(GetFullPath(@"..\data\ppt\a.pptx"));
+        this.playList.Items.Add(GetFullPath(@"..\data\ppt\b.pptx"));
+        this.playList.SelectedIndex = 0;
+    }
+
+    private static string GetFullPath(string file) =>
+        Path.Combine(Directory.GetCurrentDirectory(), file);
+
+    private void MainWindow_Closing(object? sender, System.ComponentModel.CancelEventArgs e)
+    {
+        this.presentation?.Close();
+        this.presentation = null;
+        this.app?.Quit();
+        this.app = null!;
+        GC.Collect();
+    }
+
+    private void ButtonStart_Click(object sender, RoutedEventArgs e)
+    {
+        this.StartSlideShow();
+    }
+
+    private void StartSlideShow()
+    {
+        if (this.playList.SelectedItem != null)
+        {
+            this.presentation = app.Presentations.Open(this.playList.SelectedItem.ToString());
+            var slideShowSettings = presentation.SlideShowSettings;
+            slideShowSettings.Run();
+        }
+    }
+
+    private void App_SlideShowEnd(Presentation Pres)
+    {
+        this.presentation?.Close();
+        this.presentation = null;
+
+        Dispatcher.Invoke(() =>
+        {
+            if (this.playList.SelectedIndex < this.playList.Items.Count - 1)
             {
-               Visible = MsoTriState.msoTrue,
-               WindowState = PpWindowState.ppWindowMinimized,
-            };
-            app.SlideShowEnd += this.App_SlideShowEnd;
-
-            this.playList.Items.Add(GetFullPath(@"..\data\ppt\a.pptx"));
-            this.playList.Items.Add(GetFullPath(@"..\data\ppt\b.pptx"));
-            this.playList.SelectedIndex = 0;
-        }
-
-        private static string GetFullPath(string file) =>
-            Path.Combine(Directory.GetCurrentDirectory(), file);
-
-        private void MainWindow_Closing(object? sender, System.ComponentModel.CancelEventArgs e)
-        {
-            this.presentation?.Close();
-            this.presentation = null;
-            this.app?.Quit();
-            this.app = null!;
-            GC.Collect();
-        }
-
-        private void ButtonStart_Click(object sender, RoutedEventArgs e)
-        {
-            this.StartSlideShow();
-        }
-
-        private void StartSlideShow()
-        {
-            if (this.playList.SelectedItem != null)
-            {
-                this.presentation = app.Presentations.Open(this.playList.SelectedItem.ToString());
-                var slideShowSettings = presentation.SlideShowSettings;
-                slideShowSettings.Run();
+                this.playList.SelectedIndex++;
+                this.StartSlideShow();
             }
-        }
+        });
+    }
 
-        private void App_SlideShowEnd(Presentation Pres)
-        {
-            this.presentation?.Close();
-            this.presentation = null;
-
-            Dispatcher.Invoke(() => {
-                if (this.playList.SelectedIndex < this.playList.Items.Count - 1)
-                {
-                    this.playList.SelectedIndex++;
-                    this.StartSlideShow();
-                }
-            });
-        }
-
-        private void ButtonNext_Click(object sender, RoutedEventArgs e)
-        {
-            this.presentation?.SlideShowWindow.View.Next();
-        }
+    private void ButtonNext_Click(object sender, RoutedEventArgs e)
+    {
+        this.presentation?.SlideShowWindow.View.Next();
     }
 }

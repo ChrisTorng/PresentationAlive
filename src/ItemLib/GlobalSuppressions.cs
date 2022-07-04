c42090eb069a7449d2788252fa0d57da0de9014b
ChrisTorng <christorng@hotmail.com>
2022-07-04 14:02:38 +0800

    ItemLib\IItem add static Open/Close and instance Stop function. Ignore CA1716

---
+++ src/ItemLib/GlobalSuppressions.cs
@@ -0,0 +1,4 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Naming", "CA1716:Identifiers should not match keywords", Justification = "<Pending>", Scope = "member", Target = "~M:PresentationAlive.ItemLib.IItem.Next")]
+[assembly: SuppressMessage("Naming", "CA1716:Identifiers should not match keywords", Justification = "<Pending>", Scope = "member", Target = "~M:PresentationAlive.ItemLib.IItem.Stop")]
